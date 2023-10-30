5cd45323ad23550aedc4b04eefa4e46188c5eede
Leonel Silvera <leonelsilvera9@gmail.com>
2023-10-29 23:21:29 -0300

    change

--- includes/app.php
+++ includes/app.php
@@ -1,11 +1,7 @@
 <?php 
-use Model\ActiveRecord;
 require __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
 require 'funciones.php';
-
-// Conectarnos a la base de datos
-ActiveRecord::setDB($db);
